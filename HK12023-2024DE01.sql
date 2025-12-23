3d4e07d06fc153678fc426c75e24e5659dca0bc8
todicode <nguyentrangiabao7100@gmail.com>
2025-12-23 16:03:39 +0700

    add answers for some final practical test

---
+++ HK12023-2024DE01.sql
@@ -0,0 +1,179 @@
+﻿CREATE DATABASE QLSV
+USE QLSV
+
+CREATE TABLE SINHVIEN (
+	MaSV VARCHAR(10) PRIMARY KEY,
+	HoTen VARCHAR(50),
+	NamSinh SMALLINT,
+	MaLop VARCHAR(10)
+)
+
+CREATE TABLE KHOA (
+	MaKhoa VARCHAR(10) PRIMARY KEY,
+	TenKhoa VARCHAR(50)
+)
+
+CREATE TABLE DANGKY (
+	MaSV VARCHAR(10),
+	MaMH VARCHAR(10),
+	HocKy TINYINT,
+	NamHoc SMALLINT
+	PRIMARY KEY(MaSV, MaMH, HocKy, NamHoc)
+)
+
+CREATE TABLE MON (
+	MaMH VARCHAR(10) PRIMARY KEY,
+	TenMH VARCHAR(50),
+	SoTC TINYINT,
+	MaKhoa VARCHAR(10)
+)
+
+ALTER TABLE DANGKY
+ADD CONSTRAINT FK_MaSV
+FOREIGN KEY (MaSV)
+REFERENCES SINHVIEN(MaSV)
+
+ALTER TABLE DANGKY
+ADD CONSTRAINT FK_MaMH
+FOREIGN KEY (MaMH)
+REFERENCES MON (MaMH)
+
+ALTER TABLE MON
+ADD CONSTRAINT FK_MaKhoa
+FOREIGN KEY (MaKhoa)
+REFERENCES KHOA (MaKhoa)
+
+-- Số tín chỉ của một môn học phải nằm trong khoảng từ 2 đến 4
+ALTER TABLE MON
+ADD CONSTRAINT CHECK_TC
+CHECK (SoTC IN(2,3,4))
+
+-- Năm đăng ký học của một sinh viên phải lớn hơn năm sinh của sinh viên đó.
+GO
+CREATE OR ALTER TRIGGER trg_namhoc
+ON DANGKY
+FOR INSERT, UPDATE
+AS
+BEGIN
+	IF EXISTS (
+		SELECT 1
+		FROM inserted i
+		JOIN SINHVIEN SV ON SV.MaSV = i.MaSV
+		WHERE i.NamHoc <= SV.NamSinh
+	)
+	BEGIN
+		PRINT N'Năm đăng ký học của một sinh viên phải lớn hơn năm sinh của sinh viên đó'
+		ROLLBACK TRANSACTION
+	END
+END
+
+-- Liệt kê danh sách các sinh viên lớp “HTCL2022”. Sắp xếp theo họ tên tăng dần.
+SELECT MaSV, HoTen
+FROM SINHVIEN
+WHERE MaLop = 'HTCL2022'
+ORDER BY HoTen ASC
+
+-- Thống kê số lượng môn học đăng ký của từng sinh viên trong học kỳ 1 năm 2023.
+-- Thông tin thống kê gồm: mã sinh viên, tên sinh viên và số lượng môn học đăng ký.
+SELECT SV.MaSV, SV.HoTen, COUNT (DK.MaMH) AS SoLuongMonHoc
+FROM SINHVIEN SV
+LEFT JOIN DANGKY DK ON SV.MaSV = DK.MaSV
+WHERE DK.HocKy = 1
+AND Dk.NamHoc = 2023
+GROUP BY SV.MaSV, SV.HoTen
+
+-- Liệt kê những sinh viên lớp “HTCL2021” chưa đăng ký học môn “Hệ quản trị Cơ
+--sở dữ liệu” trong năm 2023
+SELECT SV.MaSV, SV.HoTen
+FROM SINHVIEN SV
+WHERE SV.MaLop = 'HTCL2021'
+EXCEPT
+SELECT SV.MaSV, SV.HoTen
+FROM SINHVIEN SV
+JOIN DANGKY DK ON SV.MaSV = DK.MaSV
+JOIN MON M ON M.MaMH =DK.MaMH
+WHERE SV.MaLop = 'HTCL2021'
+AND M.TenMH = N'Hệ quản trị cơ sở dữ liệu'
+AND DK.NamHoc = 2023
+-- Liệt kê sinh viên (HoTen) lớp “HTCL2021” đăng ký học môn “Hệ quản trị Cơ sở
+-- dữ liệu” và “Lập trình Java” trong học kỳ 2 năm 2023
+SELECT SV.HoTen
+FROM SINHVIEN SV
+JOIN DANGKY DK ON SV.MaSV = DK.MaSV
+JOIN MON M ON M.MaMH = DK.MaMH
+WHERE SV.MaLop = 'HTCL2021'
+AND M.TenMH = N'Hệ quản trị cơ sở dữ liệu'
+AND DK.HocKy = 2
+AND NamHoc = 2023
+INTERSECT
+SELECT SV.HoTen
+FROM SINHVIEN SV
+JOIN DANGKY DK ON SV.MaSV = DK.MaSV
+JOIN MON M ON M.MaMH = DK.MaMH
+WHERE SV.MaLop = 'HTCL2021'
+AND M.TenMH = N'Lập trình Java'
+AND DK.HocKy = 2
+AND NamHoc = 2023
+
+-- Tìm sinh viên (HoTen) đã đăng ký học tất cả các môn của Khoa ‘Hệ thống thông
+-- tin’.
+SELECT SV.HoTen
+FROM SINHVIEN SV
+JOIN DANGKY DK On SV.MaSV = DK.MaSV
+JOIN MON M ON M.MaMH = DK.MaMH
+JOIN KHOA K ON K.MaKhoa = M.MaKhoa
+WHERE K.TenKhoa = N'Hệ thống thông tin'
+GROUP BY SV.MaSV, SV.HoTen
+HAVING COUNT (DISTINCT DK.MaMH) = (
+	SELECT COUNT (M2.MaMH)
+	FROM MON M2
+	JOIN KHOA K2 ON K2.MaKhoa = M2.MaKhoa
+	WHERE K2.TenKhoa = N'Hệ thống thông tin'
+)
+
+-- Tìm môn học (MaMH, TenMH) được sinh viên đăng ký học nhiều nhất trong học
+-- kỳ 1 năm 2023
+-- cách 1
+SELECT MH.MaMH, MH.TenMH
+FROM MON MH
+JOIN DANGKY DK ON MH.MaMH = DK.MaMH
+WHERE DK.HocKy = 1
+AND DK.NamHoc = 2023
+GROUP BY MH.MaMH, MH.TenMH
+HAVING COUNT (DK.MaSV) >= ALL (
+	SELECT COUNT (DK2.MaSV)
+	FROM DANGKY DK2
+	WHERE DK2.HocKy = 1
+	AND DK2.NamHoc = 2023
+	GROUP BY DK2.MaMH
+)
+
+--cách 2
+SELECT TOP 1 WITH TIES MH.MaMH, MH.TenMH, COUNT(DK.MaSV) as SoLuong
+FROM MON MH
+JOIN DANGKY DK ON MH.MaMH = DK.MaMH
+WHERE DK.HocKy = 1 AND DK.NamHoc = 2023
+GROUP BY MH.MaMH, MH.TenMH
+ORDER BY COUNT(DK.MaSV) DESC
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ HK12023-2024DE02.sql
@@ -0,0 +1,173 @@
+﻿CREATE DATABASE QLCS
+USE QLCS
+
+CREATE TABLE CASI (
+	MaCS CHAR(5) PRIMARY KEY,
+	HoTen VARCHAR(30),
+	NgaySinh SMALLDATETIME,
+	SoDT VARCHAR(15)
+)
+
+CREATE TABLE BAIHAT (
+	MaBH CHAR(5) PRIMARY KEY,
+	TenBH VARCHAR(25),
+	NgayST SMALLDATETIME,
+	MaTG CHAR(5)
+)
+
+CREATE TABLE TACGIA (
+	MaTG CHAR(5) PRIMARY KEY,
+	HoTen VARCHAR(20),
+	NgSinh SMALLDATETIME,
+	SoDT INT
+)
+
+CREATE TABLE LICHDIEN (
+	NgayDien SMALLDATETIME,
+	MaCS CHAR(5),
+	MaBH CHAR(5),
+	PRIMARY KEY (NgayDien, MaCS, MaBH),
+	DiaDiem VARCHAR(50),
+	TenSK VARCHAR(20)
+)
+
+ALTER TABLE BAIHAT
+ADD CONSTRAINT FK_MaTG
+FOREIGN KEY (MaTG)
+REFERENCES TACGIA(MaTG)
+
+ALTER TABLE LICHDIEN
+ADD CONSTRAINT FK_MaCS
+FOREIGN KEY (MaCS)
+REFERENCES CASI(MaCS)
+
+ALTER TABLE LICHDIEN
+ADD CONSTRAINT FK_MaBH
+FOREIGN KEY (MaBH)
+REFERENCES BAIHAT(MaBH)
+
+-- Trong cùng một ngày diễn, mỗi ca sĩ phải hát ít nhất 2 bài trở lên
+GO
+CREATE TRIGGER TG_2a ON LICHDIEN FOR INSERT, UPDATE
+AS 
+BEGIN 
+    IF EXISTS (
+        SELECT LD.MaCS, LD.NgayDien
+        FROM LICHDIEN LD
+        JOIN INSERTED I ON LD.MaCS = I.MaCS AND LD.NgayDien = I.NgayDien
+        GROUP BY LD.MaCS, LD.NgayDien
+        HAVING COUNT(*) < 2
+    )
+    BEGIN 
+        PRINT N'Mỗi ca sĩ phải hát ít nhất 2 bài trong một ngày diễn'
+        ROLLBACK TRAN
+    END
+END
+-- Ngày diễn của một ca sĩ phải lớn hơn ngày sinh của ca sĩ đó. 
+GO
+CREATE OR ALTER TRIGGER trg_ngaydien2
+ON LICHDIEN
+FOR INSERT, UPDATE
+AS
+BEGIN
+	IF EXISTS (
+		SELECT *
+		FROM inserted I
+		JOIN CASI CS ON I.MaCS = CS.MaCS
+		WHERE I.NgayDien <= CS.NgaySinh
+	)
+	BEGIN
+		PRINT N'Ngày diễn của một ca sĩ phải lớn hơn ngày sinh'
+		ROLLBACK TRANSACTION
+	END
+END
+
+-- Liệt kê các bài hát (MaBH, TenBH) do nhạc sĩ ‘Trịnh Công Sơn’ sáng tác được biểu
+-- diễn trong đêm nhạc ngày ‘26/09/2021’. Kết quả xuất ra theo tên bài hát có thứ tự
+-- tăng dần
+
+SELECT BH.MaBH, BH.TenBH
+FROM BAIHAT BH
+JOIN TACGIA TG ON BH.MaTG = TG.MaTG
+JOIN LICHDIEN LD ON LD.MaBH = BH.MaBH
+WHERE TG.HoTen = N'Trịnh Công Sơn'
+AND LD.NgayDien = '2021-09-26'
+ORDER BY BH.TenBH ASC
+
+-- Tìm tác giả (MaTG,HoTen) đã sáng tác bài hát ‘Nối vòng tay lớn’ được ca sĩ ‘Tạ
+-- Minh Tâm’ trình bày trong đêm nhạc ngày ‘26/09/2021’. 
+
+SELECT TG.MaTG, TG.HoTen
+FROM TACGIA TG
+JOIN BAIHAT BH ON TG.MaTG = BH.MaTG
+JOIN LICHDIEN LD ON LD.MaBH = BH.MaBH
+JOIN CASI CS ON CS.MaCS = LD.MaCS
+WHERE CS.HoTen = N'Tạ Minh Tâm'
+AND LD.NgayDien = '2021-09-26'
+AND BH.TenBH = N'Nối vòng tay lớn'
+
+-- Thống kê số lượng bài hát trình bày của từng ca sĩ trong đêm nhạc ngày ‘26/09/2021’.
+-- Thông tin thống kê gồm: mã ca sĩ, tên ca sĩ và số lượng bài hát trình bày
+
+SELECT CS.MaCS, CS.HoTen, COUNT(LD.MaBH) AS SoLuongBaiHat
+FROM LICHDIEN LD
+JOIN CASI CS ON CS.MaCS = LD.MaCS
+WHERE LD.NgayDien = '2021-09-26'
+GROUP BY CS.MaCS, CS.HoTen
+
+-- Liệt kê ca sĩ (HoTen) trong đêm nhạc ngày ‘22/12/2023’ tại ‘sân vận động Mỹ Đình
+-- Hà Nội’ (TenSK) đã trình diễn bài hát của nhạc sĩ ‘Văn Cao’ nhưng không trình
+-- diễn bài hát của nhạc sĩ ‘Hoàng Quý’.
+
+SELECT CS.HoTen
+FROM CASI CS
+JOIN LICHDIEN LD ON LD.MaCS = CS.MaCS
+JOIN BAIHAT BH ON BH.MaBH = LD.MaBH
+JOIN TACGIA TG ON TG.MaTG = BH.MaTG
+WHERE LD.NgayDien = '2023-12-22'
+AND LD.TenSK = N'sân vận động Mỹ Đình Hà Nội'
+AND TG.HoTen = N'Văn Cao'
+EXCEPT
+SELECT CS.HoTen
+FROM CASI CS
+JOIN LICHDIEN LD ON LD.MaCS = CS.MaCS
+JOIN BAIHAT BH ON BH.MaBH = LD.MaBH
+JOIN TACGIA TG ON TG.MaTG = BH.MaTG
+WHERE LD.NgayDien = '2023-12-22'
+AND LD.TenSK = N'sân vận động Mỹ Đình Hà Nội'
+AND TG.HoTen = N'Hoàng Quý'
+
+-- Tìm ca sĩ (HoTen) đã hát tất cả các bài hát do nhạc sĩ ‘Trịnh Công Sơn’ sáng tác sau
+-- năm 1990. 
+SELECT CS.HoTen
+FROM CASI CS
+JOIN LICHDIEN LD ON LD.MaCS = CS.MaCS
+JOIN BAIHAT BH ON BH.MaBH = LD.MaBH
+JOIN TACGIA TG ON TG.MaTG = BH.MaTG
+WHERE TG.HoTen = N'Trịnh Công Sơn'
+AND YEAR(BH.NgayST) > 1990
+GROUP BY CS.HoTen, CS.MaCS
+HAVING COUNT (DISTINCT BH.MaBH) = (
+	SELECT COUNT (BH2.MaBH)
+	FROM BAIHAT BH2
+	JOIN TACGIA TG2 ON TG2.MaTG = BH2.MaTG
+	WHERE TG2.HoTen = N'Trịnh Công Sơn'
+	AND YEAR (BH2.NgayST) > 1990
+)
+
+-- Tìm ca sĩ (MaCS,HoTen) đã hát nhiều bài hát nhất của nhạc sĩ ‘Trịnh Công Sơn
+SELECT CS.MaCS, CS.HoTen
+FROM CASI CS
+JOIN LICHDIEN LD ON LD.MaCS = CS.MaCS
+JOIN BAIHAT BH ON BH.MaBH = LD.MaBH
+JOIN TACGIA TG ON TG.MaTG = BH.MaTG
+WHERE TG.HoTen = N'Trịnh Công Sơn'
+GROUP BY CS.MaCS, CS.HoTen
+HAVING COUNT (DISTINCT BH.MaBH) >= ALL (
+	SELECT COUNT (LD2.MaBH)
+	FROM LICHDIEN LD2
+	JOIN BAIHAT BH2 ON LD2.MaBH = BH2.MaBH
+	JOIN TACGIA TG2 ON TG2.MaTG = BH2.MaTG
+	WHERE TG2.HoTen = N'Trịnh Công Sơn'
+	GROUP BY LD2.MaCS
+)
